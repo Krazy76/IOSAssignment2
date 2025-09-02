3e6d3c4d076cd5190c553c4b0f6c57610ff50e5d
Wilson Wong <arkeanwong@gmail.com>
2025-09-02 22:31:08 +1000

    added an icon for UI purposes

--- Enums.swift
+++ Enums.swift
@@ -37,4 +37,15 @@ enum TaskCategory: String, CaseIterable, Codable {
     case health = "Health"
     case finance = "Finance"
     case other = "Other"
+    
+    var icon: String {
+           switch self {
+           case .personal: return "person.fill"
+           case .work: return "briefcase.fill"
+           case .shopping: return "cart.fill"
+           case .health: return "heart.fill"
+           case .finance: return "dollarsign.circle.fill"
+           case .other: return "folder.fill"
+        }
+    }
 }
