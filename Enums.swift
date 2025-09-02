9257f4614fc50b72978f35077b70dc975c28056b
Wilson Wong <arkeanwong@gmail.com>
2025-09-02 22:17:00 +1000

    added a task priority enums

---
+++ Enums.swift
@@ -0,0 +1,22 @@
+//
+//  Enums.swift
+//
+//
+//  Created by Wong Wilson on 2/9/2025.
+//
+
+
+enum TaskPriority: String, CaseIterable, Codable {
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+    case urgent = "Urgent"
+    
+    var color: Color {
+        switch self {
+        case .low: return .green
+        case .medium: return .yellow
+        case .high: return .orange
+        case .urgent: return .red
+        }
+    }
